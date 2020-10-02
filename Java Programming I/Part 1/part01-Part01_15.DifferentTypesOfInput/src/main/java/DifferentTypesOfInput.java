1241a21475ba9d12eab657b7271c632e27c2da51
markzarak <zarak@sheridancollege.ca>
2020-10-02 14:15:00 -0400

    Different Types of Input

--- Java Programming I/Part 1/part01-Part01_15.DifferentTypesOfInput/src/main/java/DifferentTypesOfInput.java
+++ Java Programming I/Part 1/part01-Part01_15.DifferentTypesOfInput/src/main/java/DifferentTypesOfInput.java
@@ -1,3 +1,7 @@
+/*
+ * Different Types of Input
+ * @author: Mark Zarak, Oct 2020
+ */
 
 import java.util.Scanner;
 
@@ -6,7 +10,18 @@ public class DifferentTypesOfInput {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give a string:");
+        String string = scan.nextLine();
+        System.out.println("Give an integer:");
+        int num1 = Integer.valueOf(scan.nextLine());
+        System.out.println("Give a double:");
+        double num2 = Double.valueOf(scan.nextLine());
+        System.out.println("Give a boolean:");
+        boolean trueOrFalse = Boolean.valueOf(scan.nextLine());
+        System.out.println("You gave the string " + string);
+        System.out.println("You gave the integer " + num1);
+        System.out.println("You gave the double " + num2);
+        System.out.println("You gave the boolean " + trueOrFalse);
 
     }
 }
