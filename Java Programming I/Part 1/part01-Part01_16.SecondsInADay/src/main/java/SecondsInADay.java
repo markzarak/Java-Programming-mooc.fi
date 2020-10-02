31190f091762d00eb75c70adfb4251acd04cef8a
markzarak <zarak@sheridancollege.ca>
2020-10-02 16:35:09 -0400

    Calculating with numbers - Precedence and Parentheses

--- Java Programming I/Part 1/part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
+++ Java Programming I/Part 1/part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
@@ -1,3 +1,7 @@
+/*
+ * Calculating with numbers - Precedence and Parentheses
+ * @author: Mark Zarak, Oct 2020
+ */
 
 import java.util.Scanner;
 
@@ -6,7 +10,10 @@ public class SecondsInADay {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("How many days would you like to convert to seconds?");
+        int days = Integer.valueOf(scanner.nextLine());
+        int seconds = days * 86400;
+        System.out.println(seconds);
 
     }
 }
