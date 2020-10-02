7a6066f302c84b9c234ef1547e4bd8b3dfb1d1ee
markzarak <zarak@sheridancollege.ca>
2020-10-02 02:33:56 -0400

    Concatenation - Joining Strings Together

---
+++ part01-Part01_06.HiAdaLovelace/src/main/java/HiAdaLovelace.java
@@ -0,0 +1,12 @@
+/*
+ * Concatenation - Joining Strings Together
+ * @author: Mark Zarak, Oct 2020
+ */
+
+public class HiAdaLovelace {
+
+    public static void main(String[] args) {
+        String name = "Ada Lovelace";
+        System.out.println("Hi " + name + "!");
+    }
+}
