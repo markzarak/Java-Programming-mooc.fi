b8b7cba0b759eb0daa5f02fabcd102cac79fd9fe
markzarak <zarak@sheridancollege.ca>
2020-10-02 03:55:24 -0400

    In Progress

---
+++ Java Programming I/Part 1/part01-Part01_13.DoubleInput/src/main/java/DoubleInput.java
@@ -0,0 +1,16 @@
+/*
+ * 
+ * @author: Mark Zarak, Oct 2020
+ */
+
+import java.util.Scanner;
+
+public class DoubleInput {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // write your program here
+
+    }
+}
