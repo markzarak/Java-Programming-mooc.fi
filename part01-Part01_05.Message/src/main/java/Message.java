d1432145ce3eaa069e004e7241166adcc74920f2
markzarak <zarak@sheridancollege.ca>
2020-10-02 02:31:31 -0400

    Reading input

---
+++ part01-Part01_05.Message/src/main/java/Message.java
@@ -0,0 +1,17 @@
+/**
+ * Printing Reading input
+ * @author: Mark Zarak, Oct 2020
+ */
+
+import java.util.Scanner;
+
+public class Message {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a message:");
+        String message = scanner.nextLine();
+        System.out.println(message);
+    }
+}
