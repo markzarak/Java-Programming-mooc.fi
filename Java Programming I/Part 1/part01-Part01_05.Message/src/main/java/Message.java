85df9e8096296e7cc47c3e476dcb52e0dfa4503b
markzarak <66150681+markzarak@users.noreply.github.com>
2020-10-02 04:04:51 -0400

    Printing Reading input

--- Java Programming I/Part 1/part01-Part01_05.Message/src/main/java/Message.java
+++ Java Programming I/Part 1/part01-Part01_05.Message/src/main/java/Message.java
@@ -9,7 +9,6 @@ public class Message {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Write a message:");
         String message = scanner.nextLine();
         System.out.println(message);
