4d2e41b8a1bd61f0c3f24e946dc08511733bd4c1
markzarak <zarak@sheridancollege.ca>
2020-10-02 16:36:52 -0400

    Division

--- Java Programming I/Part 1/part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
+++ Java Programming I/Part 1/part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
@@ -1,3 +1,7 @@
+/*
+ * Division
+ * @author: Mark Zarak, Oct 2020
+ */
 
 import java.util.Scanner;
 
@@ -6,7 +10,13 @@ public class AverageOfTwoNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give the first number:");
+        double num1 = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        double num2 = Integer.valueOf(scanner.nextLine());
+
+        double avg = (num1 + num2) / 2;
+        System.out.println("The average is " + avg);
 
     }
 }
--- Java Programming I/Part 1/part01-Part01_22.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
+++ Java Programming I/Part 1/part01-Part01_22.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
@@ -1,3 +1,7 @@
+/*
+ * Division
+ * @author: Mark Zarak, Oct 2020
+ */
 
 import java.util.Scanner;
 
@@ -6,7 +10,15 @@ public class AverageOfThreeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give the first number:");
+        double num1 = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        double num2 = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the third number:");
+        double num3 = Integer.valueOf(scanner.nextLine());
+
+        double avg = (num1 + num2 + num3) / 3;
+        System.out.println("The average is " + avg);
 
     }
 }
