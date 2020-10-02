3a2588f9ea3d11fea6ae15cafa4d5da5206e2981
markzarak <zarak@sheridancollege.ca>
2020-10-02 03:23:53 -0400

    Program Execution Waits for Input

---
+++ part01-Part01_09.Conversation/src/main/java/Conversation.java
@@ -0,0 +1,20 @@
+/*
+ * Program Execution Waits for Input
+ * @author: Mark Zarak, Oct 2020
+ */
+
+import java.util.Scanner;
+
+public class Conversation {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Greetings! How are you doing?");
+        scanner.nextLine();
+        System.out.println("Oh, how interesting. Tell me more!");
+        scanner.nextLine();
+        System.out.println("Thanks for sharing!");
+
+    }
+}
