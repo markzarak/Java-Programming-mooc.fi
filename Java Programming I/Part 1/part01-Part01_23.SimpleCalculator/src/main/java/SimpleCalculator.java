d72f70efe8c94173d2902ccc24db485f94cda646
markzarak <zarak@sheridancollege.ca>
2020-10-02 16:37:06 -0400

    Simple calculator

--- Java Programming I/Part 1/part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
+++ Java Programming I/Part 1/part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -1,3 +1,7 @@
+/*
+ * Simple calculator
+ * @author: Mark Zarak, Oct 2020
+ */
 
 import java.util.Scanner;
 
@@ -6,7 +10,15 @@ public class SimpleCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give the first number:");
+        int num1 = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int num2 = Integer.valueOf(scanner.nextLine());
+
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+        System.out.println(num1 + " / " + num2 + " = " + ((double) num1 / num2));
 
     }
 }
