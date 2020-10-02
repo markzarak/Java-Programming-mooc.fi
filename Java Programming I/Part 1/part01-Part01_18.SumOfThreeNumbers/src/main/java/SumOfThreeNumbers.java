444552a0ab993c205c00c08d71e6cb8d8bb307d4
markzarak <zarak@sheridancollege.ca>
2020-10-02 16:36:17 -0400

    Calculating and Printing

--- Java Programming I/Part 1/part01-Part01_18.SumOfThreeNumbers/src/main/java/SumOfThreeNumbers.java
+++ Java Programming I/Part 1/part01-Part01_18.SumOfThreeNumbers/src/main/java/SumOfThreeNumbers.java
@@ -1,3 +1,7 @@
+/*
+ * Calculating and Printing
+ * @author: Mark Zarak, Oct 2020
+ */
 
 import java.util.Scanner;
 
@@ -6,7 +10,15 @@ public class SumOfThreeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give the first number:");
+        int num1 = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int num2 = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the third number:");
+        int num3 = Integer.valueOf(scanner.nextLine());
+
+        int sum = num1 + num2 + num3;
+        System.out.println("The sum of the numbers is " + sum);
 
     }
 }
