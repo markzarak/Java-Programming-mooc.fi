f00556abea197b2d80d525d33d3894bf79023e60
markzarak <zarak@sheridancollege.ca>
2020-10-03 13:26:41 -0400

    More Conditionals: else if

--- Java Programming I/Part 1/part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
+++ Java Programming I/Part 1/part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
@@ -1,3 +1,7 @@
+/*
+ * More Conditionals: else if
+ * @author: Mark Zarak, Oct 2020
+ */
 
 import java.util.Scanner;
 
@@ -6,5 +10,16 @@ public class LargerThanOrEqualTo {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("Give the first number:");
+        int num1 = Integer.valueOf(scan.nextLine());
+        System.out.println("Give the second number:");
+        int num2 = Integer.valueOf(scan.nextLine());
+        if (num1 > num2) {
+            System.out.println("Greater number is: " + num1);
+        } else if (num2 > num1) {
+            System.out.println("Greater number is: " + num2);
+        } else {
+            System.out.println("The numbers are equal!");
+        }
     }
 }
