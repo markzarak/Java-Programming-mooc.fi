971543bd8d53d69e74f40dc468138b8d3bd1439d
markzarak <zarak@sheridancollege.ca>
2020-10-03 13:27:17 -0400

    Conditional Statement Expression and the Boolean Variable

--- Java Programming I/Part 1/part01-Part01_32.OddOrEven/src/main/java/OddOrEven.java
+++ Java Programming I/Part 1/part01-Part01_32.OddOrEven/src/main/java/OddOrEven.java
@@ -1,3 +1,7 @@
+/*
+ * Conditional Statement Expression and the Boolean Variable
+ * @author: Mark Zarak, Oct 2020
+ */
 
 import java.util.Scanner;
 
@@ -6,19 +10,13 @@ public class OddOrEven {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here 
-        // HINT:
-        // You can find out if a number is even or odd easily using the modulo operator %
-        // Try the following commands to see what they print
-        // System.out.println( 1%2 );
-        // System.out.println( 2%2 );
-        // System.out.println( 3%2 );
-        // System.out.println( 4%2 );
-        // System.out.println( 5%2 );
-        // System.out.println( 6%2 );
-        // System.out.println( 7%2 );
-        // int luku = 8
-        // System.out.println( luku%2 );
-        // So, by taking the modulo of a number and two you can find out if it is even or odd !
+        System.out.println("Give a number:");
+        int num = Integer.valueOf(scan.nextLine());
+        boolean even = num % 2 == 0;
+        if (even) {
+            System.out.println("Number " + num + " is even.");
+        } else {
+            System.out.println("Number " + num + " is odd.");
+        }
     }
 }
