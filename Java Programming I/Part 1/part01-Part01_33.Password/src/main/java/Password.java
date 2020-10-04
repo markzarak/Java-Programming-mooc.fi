484b1ed450bb4c2ffc05e11ec8276ba48951642a
markzarak <zarak@sheridancollege.ca>
2020-10-04 02:26:05 -0400

    Conditional Statements and Comparing Strings

--- Java Programming I/Part 1/part01-Part01_33.Password/src/main/java/Password.java
+++ Java Programming I/Part 1/part01-Part01_33.Password/src/main/java/Password.java
@@ -1,3 +1,7 @@
+/*
+ * Conditional Statements and Comparing Strings
+ * @author: Mark Zarak, Oct 2020
+ */
 
 import java.util.Scanner;
 
@@ -6,6 +10,12 @@ public class Password {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here 
+        System.out.println("Password?");
+        String pass = scan.nextLine();
+        if (pass.equals("Caput Draconis")) {
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("Off with you!");
+        }
     }
 }
