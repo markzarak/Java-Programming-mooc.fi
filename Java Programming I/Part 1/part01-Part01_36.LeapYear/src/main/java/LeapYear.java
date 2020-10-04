728d242e9e7e846a8aa59b0138123db60e5c60f7
markzarak <zarak@sheridancollege.ca>
2020-10-04 02:27:15 -0400

    Execution Order of Conditional Statements

--- Java Programming I/Part 1/part01-Part01_36.LeapYear/src/main/java/LeapYear.java
+++ Java Programming I/Part 1/part01-Part01_36.LeapYear/src/main/java/LeapYear.java
@@ -1,3 +1,7 @@
+/*
+ * Execution Order of Conditional Statements
+ * @author: Mark Zarak, Oct 2020
+ */
 
 import java.util.Scanner;
 
@@ -6,5 +10,12 @@ public class LeapYear {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.print("Give a year: ");
+        int year = Integer.valueOf(scan.nextLine());
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println("The year is a leap year.");
+        } else {
+            System.out.println("The year is not a leap year.");
+        }
     }
 }
