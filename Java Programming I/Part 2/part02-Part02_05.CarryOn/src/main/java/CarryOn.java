fa79736a1feecaa321ba1d124779a1c894980ac6
markzarak <zarak@sheridancollege.ca>
2020-10-06 23:42:27 -0400

    Ending a Loop

---
+++ Java Programming I/Part 2/part02-Part02_05.CarryOn/src/main/java/CarryOn.java
@@ -0,0 +1,21 @@
+/*
+ * Ending a Loop
+ * @author: Mark Zarak, Oct 2020
+ */
+
+import java.util.Scanner;
+
+public class CarryOn {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Shall we carry on?");
+            String answer = scanner.nextLine();
+            if (answer.equalsIgnoreCase("no")) {
+                break;
+            }
+        }
+    }
+}
