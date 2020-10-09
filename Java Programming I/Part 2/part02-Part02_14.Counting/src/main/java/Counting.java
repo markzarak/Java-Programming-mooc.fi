216f913b8b740330c0f7d23b443d8e01a927dcd2
markzarak <zarak@sheridancollege.ca>
2020-10-08 21:04:52 -0400

    For Loop

---
+++ Java Programming I/Part 2/part02-Part02_14.Counting/src/main/java/Counting.java
@@ -0,0 +1,18 @@
+/*
+ * For Loop
+ * @author: Mark Zarak, Oct 2020
+ */
+
+import java.util.Scanner;
+
+public class Counting {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.valueOf(scanner.nextLine());
+        for (int i = 0; i <= num; ++i) {
+            System.out.println(i);
+        }
+    }
+}
