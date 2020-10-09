dec39bb48b4fcc35839a3688018b9da1beaaa4c0
markzarak <zarak@sheridancollege.ca>
2020-10-08 22:28:11 -0400

    Repeating Functionality

---
+++ Java Programming I/Part 2/part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -0,0 +1,21 @@
+/*
+ * Repeating Functionality
+ * @author: Mark Zarak, Oct 2020
+ */
+
+import java.util.Scanner;
+
+public class SumOfASequence {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Last number? ");
+        int lastNum = Integer.valueOf(scanner.nextLine());
+        int sum = 0;
+        for (int i = 0; i <= lastNum; ++i) {
+            sum += i;
+        }
+        System.out.println("The sum is " + sum);
+    }
+}
