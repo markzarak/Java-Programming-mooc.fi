66416952f9e75a922940070394b88c8479a14779
markzarak <zarak@sheridancollege.ca>
2020-10-09 00:47:44 -0400

    Method Parameters

---
+++ Java Programming I/Part 2/part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
@@ -0,0 +1,17 @@
+/*
+ * Method Parameters
+ * @author: Mark Zarak, Oct 2020
+ */
+
+public class FromOneToParameter {
+
+    public static void main(String[] args) {
+        printUntilNumber(5);
+    }
+
+    public static void printUntilNumber(int num) {
+        for (int i = 1; i <= num; ++i) {
+            System.out.println(i);
+        }
+    }
+}
