adfcb496524b47939dc98746779ea4564005bb4d
markzarak <zarak@sheridancollege.ca>
2020-10-10 21:56:55 -0400

    Methods Can Return Values

---
+++ Java Programming I/Part 2/part02-Part02_28.Word/src/main/java/Word.java
@@ -0,0 +1,15 @@
+/*
+ * Methods Can Return Values
+ * @author: Mark Zarak, Oct 2020
+ */
+
+public class Word {
+
+    public static void main(String[] args) {
+        System.out.println(word());
+    }
+
+    public static String word() {
+        return "Hello, world.";
+    }
+}
