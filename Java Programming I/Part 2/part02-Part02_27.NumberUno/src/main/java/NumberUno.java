18712ce2cd189e2b5af315c37bf346d7213fef14
markzarak <zarak@sheridancollege.ca>
2020-10-10 21:56:30 -0400

    Methods Can Return Values

---
+++ Java Programming I/Part 2/part02-Part02_27.NumberUno/src/main/java/NumberUno.java
@@ -0,0 +1,15 @@
+/*
+ * Methods Can Return Values
+ * @author: Mark Zarak, Oct 2020
+ */
+
+public class NumberUno {
+
+    public static void main(String[] args) {
+        System.out.println(numberUno());
+    }
+
+    public static int numberUno() {
+        return 1;
+    }
+}
