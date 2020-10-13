e7dd0c92eada7d4612f9c39d0c62aa6d2ba24de0
markzarak <zarak@sheridancollege.ca>
2020-10-12 22:43:40 -0400

    Adding to a List and Retrieving a Value from a Specific Place

---
+++ Java Programming I/Part 3/part03-Part03_02.SecondPlusThird/src/main/java/SecondPlusThird.java
@@ -0,0 +1,24 @@
+/*
+ * Adding to a List and Retrieving a Value from a Specific Place
+ * @author: Mark Zarak, Oct 2020
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SecondPlusThird {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number == 0) {
+                break;
+            }
+            numbers.add(number);
+        }
+        System.out.println(numbers.get(1) + numbers.get(2));
+    }
+}
