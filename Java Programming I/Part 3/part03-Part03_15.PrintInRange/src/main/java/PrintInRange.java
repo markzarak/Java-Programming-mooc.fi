35a9bdfd5341f3fe163b75fa0c3231c60064a377
markzarak <zarak@sheridancollege.ca>
2020-10-15 00:28:27 -0400

    List as a Method Parameter

---
+++ Java Programming I/Part 3/part03-Part03_15.PrintInRange/src/main/java/PrintInRange.java
@@ -0,0 +1,34 @@
+/*
+ * List as a Method Parameter
+ * @author: Mark Zarak, Oct 2020
+ */
+
+import java.util.ArrayList;
+
+public class PrintInRange {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(-1);
+        numbers.add(5);
+        numbers.add(1);
+
+        System.out.println("The numbers in the range [0, 5]");
+        printNumbersInRange(numbers, 0, 5);
+
+        System.out.println("The numbers in the range [3, 10]");
+        printNumbersInRange(numbers, 3, 10);
+    }
+
+    // Print numbers in range
+    public static void printNumbersInRange(ArrayList<Integer> numbers, int lowerLimit, int upperLimit) {
+        for (int number : numbers) {
+            if (number <= upperLimit && number >= lowerLimit) {
+                System.out.println(number);
+            }
+        }
+    }
+}
