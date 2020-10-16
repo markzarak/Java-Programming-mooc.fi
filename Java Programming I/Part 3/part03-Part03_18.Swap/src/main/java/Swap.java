791928f433b8aa5df98555209fa3c4612b79cf1e
markzarak <zarak@sheridancollege.ca>
2020-10-16 15:31:00 -0400

    Assigning and accessing elements

---
+++ Java Programming I/Part 3/part03-Part03_18.Swap/src/main/java/Swap.java
@@ -0,0 +1,42 @@
+/*
+ * Assigning and accessing elements
+ * @author: Mark Zarak, Oct 2020
+ */
+
+import java.util.Scanner;
+
+public class Swap {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[5];
+        array[0] = 1;
+        array[1] = 3;
+        array[2] = 5;
+        array[3] = 7;
+        array[4] = 9;
+
+        int index = 0;
+        while (index < array.length) {
+            System.out.println(array[index]);
+            index++;
+        }
+        System.out.println("");
+
+        System.out.println("Give two indices to swap:");
+        int i = Integer.valueOf(scanner.nextLine());
+        int j = Integer.valueOf(scanner.nextLine());
+        
+        int swapHelper = array[i];
+        array[i] = array[j];
+        array[j] = swapHelper;
+        
+        System.out.println("");
+        index = 0;
+        while (index < array.length) {
+            System.out.println(array[index]);
+            index++;
+        }
+    }
+
+}
