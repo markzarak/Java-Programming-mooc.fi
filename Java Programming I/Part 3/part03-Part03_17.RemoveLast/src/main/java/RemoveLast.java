c20eef4f675ed807d5b89444d81a9377641bd4d0
markzarak <zarak@sheridancollege.ca>
2020-10-16 15:30:40 -0400

    On Copying the List to a Method Parameter

---
+++ Java Programming I/Part 3/part03-Part03_17.RemoveLast/src/main/java/RemoveLast.java
@@ -0,0 +1,31 @@
+/*
+ * On Copying the List to a Method Parameter
+ * @author: Mark Zarak, Oct 2020
+ */
+
+import java.util.ArrayList;
+
+public class RemoveLast {
+
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<>();
+
+        strings.add("First");
+        strings.add("Second");
+        strings.add("Third");
+
+        System.out.println(strings);
+
+        removeLast(strings);
+        removeLast(strings);
+
+        System.out.println(strings);
+    }
+
+    // Remove last item on list
+    public static void removeLast(ArrayList<String> strings) {
+        if (!strings.isEmpty()) {
+            strings.remove(strings.size() - 1);
+        }
+    }
+}
