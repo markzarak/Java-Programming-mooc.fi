680f67bda3ddce67a2ed94255a9ba450eb0e8950
markzarak <zarak@sheridancollege.ca>
2020-10-17 00:59:21 -0400

    Array as a parameter of a method

---
+++ Java Programming I/Part 3/part03-Part03_20.SumOfArray/src/main/java/SumOfArray.java
@@ -0,0 +1,21 @@
+/*
+ * Array as a parameter of a method
+ * @author: Mark Zarak, Oct 2020
+ */
+
+public class SumOfArray {
+
+    public static void main(String[] args) {
+        // You can try the method here
+        int[] array = {5, 1, 3, 4, 2};
+        System.out.println(sumOfNumbersInArray(array));
+    }
+
+    public static int sumOfNumbersInArray(int[] array) {
+        int sum = 0;
+        for (int number : array) {
+            sum += number;
+        }
+        return sum;
+    }
+}
