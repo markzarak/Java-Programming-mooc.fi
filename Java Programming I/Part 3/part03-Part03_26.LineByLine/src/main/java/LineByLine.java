f34137096d2ba329a13e2e8ef842c95c9c184a00
markzarak <zarak@sheridancollege.ca>
2020-10-20 20:28:21 -0400

    Splitting a String

---
+++ Java Programming I/Part 3/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -0,0 +1,26 @@
+/*
+ * Splitting a String
+ * @author: Mark Zarak, Oct 2020
+ */
+
+import java.util.Scanner;
+ 
+public class LineByLine {
+ 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+ 
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+ 
+            String[] parts = input.split(" ");
+            for (String part : parts) {
+                System.out.println(part);
+            }
+        }
+ 
+    }
+}
