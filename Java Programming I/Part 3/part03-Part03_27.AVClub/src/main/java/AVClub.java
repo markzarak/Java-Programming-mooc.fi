dab3f1f172535f717462354305bf49f96135a32c
markzarak <zarak@sheridancollege.ca>
2020-10-25 20:29:09 -0400

    Splitting a String

--- Java Programming I/Part 3/part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ Java Programming I/Part 3/part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -4,25 +4,26 @@
  */
 
 import java.util.Scanner;
- 
+
 public class AVClub {
- 
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
- 
+
         while (true) {
-            String input = scanner.nextLine();
-            if (input.equals("")) {
+            String userInput = scanner.nextLine();
+            // If user input is empty, then exit
+            if (userInput.equals("")) {
                 break;
             }
- 
-            String[] parts = input.split(" ");
-            for (String part : parts) {
-                if (part.contains("av")) {
-                    System.out.println(part);
+            // Create an array that holds each word, using space as the delimiter
+            String[] lines = userInput.split(" ");
+            // Print each word that contains "av"
+            for (String line : lines) {
+                if (line.contains("av")) {
+                    System.out.println(line);
                 }
             }
         }
- 
     }
 }
