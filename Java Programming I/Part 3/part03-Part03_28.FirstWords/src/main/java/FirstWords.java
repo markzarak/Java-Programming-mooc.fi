bcb247bb94d5973735fc2b1d8a92f7d1319ef3a4
markzarak <zarak@sheridancollege.ca>
2020-10-25 20:29:29 -0400

    Data of Fixed Format

--- Java Programming I/Part 3/part03-Part03_28.FirstWords/src/main/java/FirstWords.java
+++ Java Programming I/Part 3/part03-Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -1,20 +1,25 @@
+/*
+ * Data of Fixed Format
+ * @author: Mark Zarak, Oct 2020
+ */
 
 import java.util.Scanner;
- 
+
 public class FirstWords {
- 
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
- 
+
         while (true) {
-            String input = scanner.nextLine();
-            if (input.equals("")) {
+            String userInput = scanner.nextLine();
+            // If user input is empty, then exit
+            if (userInput.equals("")) {
                 break;
             }
- 
-            String[] parts = input.split(" ");
-            System.out.println(parts[0]);
+            // Create an array that holds each word, using space as the delimiter
+            String[] lines = userInput.split(" ");
+            // Print the first word
+            System.out.println(lines[0]);
         }
- 
     }
 }
