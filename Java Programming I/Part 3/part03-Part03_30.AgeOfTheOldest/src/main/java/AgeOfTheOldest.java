b29e8b6099c8b8e3e22b18bfdd25b9a8d9f33e2a
markzarak <zarak@sheridancollege.ca>
2020-10-25 20:30:02 -0400

    Using Diverse Text

--- Java Programming I/Part 3/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ Java Programming I/Part 3/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -1,27 +1,31 @@
- 
+/*
+ * Using Diverse Text
+ * @author: Mark Zarak, Oct 2020
+ */
+
 import java.util.Scanner;
- 
+
 public class AgeOfTheOldest {
- 
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
- 
+
         int oldest = -1;
- 
         while (true) {
-            String input = scanner.nextLine();
-            if (input.equals("")) {
+            String userInput = scanner.nextLine();
+            // If user input is empty, then exit
+            if (userInput.equals("")) {
                 break;
             }
- 
-            String[] parts = input.split(",");
-            int age = Integer.valueOf(parts[1]);
+            // Create an array that holds each value, using comma as the delimiter
+            String[] lines = userInput.split(",");
+            // Convert String to Integer
+            int age = Integer.valueOf(lines[1]);
             if (age > oldest) {
                 oldest = age;
             }
         }
- 
+        // Print the oldest
         System.out.println("Age of the oldest: " + oldest);
- 
     }
 }
