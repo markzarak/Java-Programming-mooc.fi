a35a8bdbee9597af45eab76bf5d4e5e92fe9d78b
markzarak <zarak@sheridancollege.ca>
2020-10-25 20:30:17 -0400

    Using Diverse Text

--- Java Programming I/Part 3/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ Java Programming I/Part 3/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -1,29 +1,33 @@
+/*
+ * Using Diverse Text
+ * @author: Mark Zarak, Oct 2020
+ */
 
 import java.util.Scanner;
- 
+
 public class NameOfTheOldest {
- 
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
- 
-        int oldest = -1;
-        String name = "";
- 
+
+        String nameOfOldest = "";
+        int ageOfOldest = -1;
         while (true) {
-            String input = scanner.nextLine();
-            if (input.equals("")) {
+            String userInput = scanner.nextLine();
+            // If user input is empty, then exit
+            if (userInput.equals("")) {
                 break;
             }
- 
-            String[] parts = input.split(",");
-            int age = Integer.valueOf(parts[1]);
-            if (age > oldest) {
-                oldest = age;
-                name = parts[0];
+            // Create an array that holds each value, using comma as the delimiter
+            String[] lines = userInput.split(",");
+            // Convert String to Integer
+            int age = Integer.valueOf(lines[1]);
+            if (age > ageOfOldest) {
+                ageOfOldest = age;
+                nameOfOldest = lines[0];
             }
         }
- 
-        System.out.println("Name of the oldest: " + name);
- 
+        // Print name of the oldest
+        System.out.println("Name of the oldest: " + nameOfOldest);
     }
 }
