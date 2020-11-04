bd468224a71138886d231b8b82767c998f165528
markzarak <zarak@sheridancollege.ca>
2020-11-03 22:31:08 -0500

    Defining a Constructor

---
+++ Java Programming I/Part 4/part04-Part04_04.Room/src/main/java/MainProgram.java
@@ -0,0 +1,16 @@
+/*
+ * Defining a Constructor
+ * @author: Mark Zarak, Nov 2020
+ */
+
+import java.util.Scanner;
+
+public class MainProgram {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Room room = new Room("PROG 32758", 20);
+    }
+
+}
---
+++ Java Programming I/Part 4/part04-Part04_04.Room/src/main/java/Room.java
@@ -0,0 +1,16 @@
+/*
+ * Defining a Constructor
+ * @author: Mark Zarak, Nov 2020
+ */
+
+public class Room {
+    
+    private String code;
+    private int seats;
+    
+    public Room(String classCode, int numberOfSeats) {
+        code = classCode;
+        seats = numberOfSeats;
+        
+    }
+}
